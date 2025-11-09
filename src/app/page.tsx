8e21c7ead0cbb350bb9934b048f07fe2cd13a87e
Amith B V <amithrao0606@gmail.com>
2025-11-09 09:07:31 +0530

    Deleted theme-toggle button.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import { ThemeToggle } from "@/components/theme-toggle";
-
 export default function Home() {
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <ThemeToggle />
     </div>
   );
 }
